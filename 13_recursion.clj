f4fbb0366a3992c8f8f5695722e02e7ae24d454c
Sam <wojtowi4@illinois.edu>
2015-03-08 19:57:22 +0100

    13 solved

--- 13_recursion.clj
+++ 13_recursion.clj
@@ -14,10 +14,18 @@
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-	 __)
+	(loop [coll coll
+				 acc ()]
+		(if (empty? coll)
+			acc
+			(recur (rest coll) (conj acc (first coll))))))
 
 (defn factorial [n]
-  __)
+	(loop [n n
+				acc 1]
+  (if (= n 1)
+		acc
+		(recur (- n 1) (* acc n)))))
 
 (meditations
   "Recursion ends with a base case"
